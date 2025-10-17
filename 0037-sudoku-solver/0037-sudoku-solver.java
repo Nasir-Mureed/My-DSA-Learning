0eea4f4c17a573afbf59c952d20d8568b60804f4
Nasir-Mureed <nasirmureed80@gmail.com>
2025-10-17 10:05:42 +0500

    Time: 181 ms (64.16%), Space: 41.4 MB (26.17%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,91 @@
+class Solution {
+    static boolean canPlace(char[][] board,int i,int j,char val)
+    {
+        for(int r=0;r<9;r++) //check in given row
+        {
+            if(board[i][r]==val) //if present can not place val
+                return false;
+        }
+        for(int c=0;c<9;c++) //check in given column
+        {
+            if(board[c][j]==val)
+                return false;
+        }
+        int r,c;  //represent the starting cell of 3 X 3 sub grid
+        if(i<=2)
+        {
+            r=0;
+            if(j<=2)     //1st Sub Grid
+                c=0;
+            else if(j<=5) //2nd Sub Grid
+                c=3;
+            else          //3rd Sub Grid
+                c=6;
+
+        }
+        else if(i<=5)
+        {
+            r=3;
+            if(j<=2)        //4th Sub Grid
+                c=0;
+            else if(j<=5)   //5th Sub Grid
+                c=3;
+            else            //6th Sub Grid
+                c=6;
+
+        }
+        else
+        {
+            r=6;
+            if(j<=2)  //7th Sub Grid
+                c=0;
+            else if(j<=5) //8th Sub Grid
+                c=3;
+            else        //9th Sub Grid
+                c=6;
+
+        }
+        for(int x=0;x<3;x++)    // check in given sub Grid
+        {
+            for(int y=0;y<3;y++)
+            { 
+                if(board[r+x][c+y]==val)
+                    return false;
+            }
+        }
+        return true;
+    }
+    static boolean solve(char[][] board,int i,int j) 
+    {
+        if(j==9)
+        {
+            j=0;
+            i++;
+        }
+        if(i>=9 )
+            return true;// sudoku solved
+        if(board[i][j]!='.')
+        {
+            if(solve(board,i,j+1)) return true;
+        }
+        else
+        {
+           for(char k='1';k<='9';k++)
+            {
+                if(canPlace(board,i,j,k))
+                {
+                    board[i][j]=k;
+                    if(solve(board,i,j+1)) return true;
+                    board[i][j]='.';
+                }
+               
+            } 
+        }
+        return false;
+        
+    }
+     public void solveSudoku(char[][] board) 
+     {
+        solve(board,0,0);
+     }
+}

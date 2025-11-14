453fb6228a723a6671672d4f142520c0a7b1e19b
Nasir-Mureed <nasirmureed80@gmail.com>
2025-11-14 18:24:01 +0500

    Time: 0 ms (100%), Space: 22.5 MB (54.74%) - LeetHub

--- 1030-smallest-string-starting-from-leaf/1030-smallest-string-starting-from-leaf.cpp
+++ 1030-smallest-string-starting-from-leaf/1030-smallest-string-starting-from-leaf.cpp
@@ -11,54 +11,34 @@
  */
 class Solution 
 {
-    string reverseCompare(string s1,string s2)
-    {
-        if(s1==s2)
-            return s1;
-        int n1=s1.size();
-        int n2=s2.size();
-        n1--;
-        n2--;
-        while(n1>=0 && n2>=0)
-        {
-            if(s1[n1]<s2[n2])
-                return s1;
-            else if(s2[n2]<s1[n1])
-                return s2;
-            else
-            {
-                n1--;
-                n2--;
-            }
-        }
-        if(n1!=-1)
-            return s2; 
-        if(n2!=-1)
-            return s1;
-        return s1;//equal
-    }
-    string helper(TreeNode* root,string str)
-    {
-        if(!root)
-            return str;
-        str+=(char(root->val+97));
-        string s1,s2;
-        s1= helper(root->left,str);
-        s2=helper(root->right,str);
-        if(s1==str) 
-            return s2;
-        if(s2==str)
-            return s1;
-       
-        return reverseCompare(s1,s2);
-
-    }
+    string smallest;
 public:
-   
+    void dfs(TreeNode* node, string path) 
+    {
+        if (!node) return;
+
+        path.push_back('a' + node->val);
+
+        //leaf
+        if (!node->left && !node->right) 
+        {
+            string temp = path;
+            reverse(temp.begin(), temp.end());
+            if (smallest.empty() || temp < smallest) 
+            {
+                smallest = temp;
+            }
+            return;
+        }
+
+        dfs(node->left, path);
+        dfs(node->right, path);
+    }
+
     string smallestFromLeaf(TreeNode* root) 
     {
-        string s=helper(root,"");
-        reverse(s.begin(),s.end());
-        return s;
+        smallest = "";
+        dfs(root, "");
+        return smallest;
     }
 };

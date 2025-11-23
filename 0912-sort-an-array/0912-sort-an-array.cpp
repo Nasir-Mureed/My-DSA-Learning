f9f62a7fac5fafa832a766d583c0e54bad65b38a
Nasir-Mureed <nasirmureed80@gmail.com>
2025-11-23 12:01:52 +0500

    Time: 594 ms (28.88%), Space: 295.5 MB (21.63%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,53 +1,99 @@
 class Solution 
 {
-    void merge(vector<int>& nums, int start,int end)
+
+    void merge(vector<int> &nums,int st,int mid,int end)
     {
-        if(start>=end) return ; // base
-        int mid = start+(end-start)/2;
-        merge(nums,start,mid); // sort left subarray
-        merge(nums,mid+1,end); // sort right sunarr
-
-        vector<int> temp1 (mid-start+1),temp2(end-mid);
-
-        for(int i=0,j=start;i<mid-start+1;i++,j++) // copy left sorted arr
-            temp1[i]=nums[j];
-        for(int i=0,j=mid+1;i<end-mid ; i++,j++) //copy right subarr
-            temp2[i]=nums[j];
-
-        int i=0,j=0,k=start;
-        while((i<mid-start+1) && (j<end-mid))
+        int l = st;
+        int r = mid+1;
+        vector<int> list;
+        while(l <= mid && r <= end)
         {
-            if(temp1[i]<temp2[j])
+            if(nums[l] <= nums[r]) 
             {
-                nums[k]=temp1[i];
-                i++;
+                list.push_back(nums[l++]);
             }
             else
             {
-                nums[k]=temp2[j];
-                j++;
+                list.push_back(nums[r++]);
             }
-            k++;
         }
-
-        while(i<mid-start+1)
+        while(l <= mid) 
         {
-            nums[k]=temp1[i];
-            i++;
-            k++;
+            list.push_back(nums[l++]);
         }
+        while(r <= end) list.push_back(nums[r++]);
 
-        while(j<end-mid)
+        for(int i = st;i<=end;i++)
         {
-            nums[k]=temp2[j];
-            j++;
-            k++;
+            nums[i] = list[i-st];
         }
     }
-public:
-    vector<int> sortArray(vector<int>& nums) 
+    void divide(vector<int> &nums,int st,int end)
     {
-        merge(nums,0,nums.size()-1);
-        return nums;        
+        if(st >= end) return;
+        int mid = (st + end) / 2;
+        divide(nums,st,mid);
+        divide(nums,mid+1,end);
+        merge(nums,st,mid,end);
     }
+    public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        // atexit([](){ std::ofstream("display_runtime.txt") << "0"; });
+        divide(nums,0,n-1);
+        return nums;
+    }
+
+
+
+//     void merge(vector<int>& nums, int start,int end)
+//     {
+//         if(start>=end) return ; // base
+//         int mid = start+(end-start)/2;
+//         merge(nums,start,mid); // sort left subarray
+//         merge(nums,mid+1,end); // sort right sunarr
+
+//         vector<int> temp1 (mid-start+1),temp2(end-mid);
+
+//         for(int i=0,j=start;i<mid-start+1;i++,j++) // copy left sorted arr
+//             temp1[i]=nums[j];
+//         for(int i=0,j=mid+1;i<end-mid ; i++,j++) //copy right subarr
+//             temp2[i]=nums[j];
+
+//         int i=0,j=0,k=start;
+//         while((i<mid-start+1) && (j<end-mid))
+//         {
+//             if(temp1[i]<temp2[j])
+//             {
+//                 nums[k]=temp1[i];
+//                 i++;
+//             }
+//             else
+//             {
+//                 nums[k]=temp2[j];
+//                 j++;
+//             }
+//             k++;
+//         }
+
+//         while(i<mid-start+1)
+//         {
+//             nums[k]=temp1[i];
+//             i++;
+//             k++;
+//         }
+
+//         while(j<end-mid)
+//         {
+//             nums[k]=temp2[j];
+//             j++;
+//             k++;
+//         }
+//     }
+// public:
+//     vector<int> sortArray(vector<int>& nums) 
+//     {
+//         merge(nums,0,nums.size()-1);
+//         return nums;        
+//     }
 };

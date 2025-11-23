9e70140af3899fb9b980e1055a97010e79300bbf
Nasir-Mureed <nasirmureed80@gmail.com>
2025-11-23 21:49:59 +0500

    Time: 10 ms (75.93%), Space: 17.5 MB (40.08%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,50 +9,65 @@
  */
 class Solution 
 {
-    bool  found(TreeNode* root , TreeNode* ans)
-    {
-        if(!root)
-            return false; //not found
-        if(root==ans)
-            return true; // found
-        bool status=found(root->left,ans);
-        if(status) // if found no need to check further
-        {
-            return true; 
-        }
-        else
-        {
-            return found(root->right,ans);
-        }
-    }
+
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-        if(root==p || root==q)
-        {
-           return root;
-        }
-        if(found(root->left,p)) // find p in left
-        {
-            if(found(root->left,q)) // find q in left 
-            {
-                return lowestCommonAncestor(root->left,p,q);
-            }
-            else // q must be right
-            {
-                return root;
-            }
-        }
-        else  // p must be in right
-        {
-            if(found(root->left,q)) // find q in left 
-            {
-                return root;
-            }
-            else // q must be right
-            {
-                return lowestCommonAncestor(root->right,p,q);
-            }
-        }
+        if(!root || root==p || root==q) return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right= lowestCommonAncestor(root->right,p,q);
+        if(left && right) return root;
+        if(!left)
+            return right;
+        else
+            return left;
     }
+
+    // brute force
+//     bool  found(TreeNode* root , TreeNode* ans)
+//     {
+//         if(!root)
+//             return false; //not found
+//         if(root==ans)
+//             return true; // found
+//         bool status=found(root->left,ans);
+//         if(status) // if found no need to check further
+//         {
+//             return true; 
+//         }
+//         else
+//         {
+//             return found(root->right,ans);
+//         }
+//     }
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+//     {
+//         if(root==p || root==q)
+//         {
+//            return root;
+//         }
+//         if(found(root->left,p)) // find p in left
+//         {
+//             if(found(root->left,q)) // find q in left 
+//             {
+//                 return lowestCommonAncestor(root->left,p,q);
+//             }
+//             else // q must be right
+//             {
+//                 return root;
+//             }
+//         }
+//         else  // p must be in right
+//         {
+//             if(found(root->left,q)) // find q in left 
+//             {
+//                 return root;
+//             }
+//             else // q must be right
+//             {
+//                 return lowestCommonAncestor(root->right,p,q);
+//             }
+//         }
+//     }
 };
